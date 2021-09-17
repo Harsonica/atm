fbc6e0075eb7e7a1c182d53afda69d0467c94d74
Harsonica <72625830+Harsonica@users.noreply.github.com>
2021-09-16 21:03:46 -0400

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,15 @@
+class Atm[object]:
+    def __init__(self,pinNumber,cardNumber):
+        self.pinNumber=pinNumber
+        self.cardNumber=cardNumber
+
+    def pin (self):
+        print("12345")
+    
+    def card (self):
+        print("1234")
+
+
+        
+        
+
